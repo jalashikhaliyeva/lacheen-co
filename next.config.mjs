ef860dd2fe98cd8e310f6f78d3479b64b56b44d1
Jala Sh <jala.shikhaliyeva@gmail.com>
2025-10-16 10:11:23 +0400

    fix problem appearing in images visibiloty on sever

--- next.config.mjs
+++ next.config.mjs
@@ -8,8 +8,14 @@ const nextConfig = {
    
   },
   images: {
-  domains: ["firebasestorage.googleapis.com"],
-}
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        pathname: '/**',
+      },
+    ],
+  }
 
 };
 
